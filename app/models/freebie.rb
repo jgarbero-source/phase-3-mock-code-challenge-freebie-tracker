8fd618c23b41283e831e43c7fb945305204a22d0
John Garbero <john.garbero.itb@gmail.com>
2022-07-05 15:45:04 -0500

    did what I could

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,16 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :dev
+    belongs_to :company
 
+    def print_details
+        dev_name = ""
+        c_name = ""
+        self.dev.collect do |d|
+            d.name = dev_name
+        end
+        self.company.collect do |c|
+            c.name = c_name
+        end
+        "#{dev_name} owns a #{self.item_name} from #{c_name}."
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,9 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "Pen", value: 3)
+Freebie.create(item_name: "Phone case", value: 10)
+Freebie.create(item_name: "Ball", value: 4)
+Freebie.create(item_name: "Slinky", value: 2)
 
 puts "Seeding done!"
